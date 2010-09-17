3e34bc934a5263b2552fe0f1e82af0bdb3c46d05
kathryn <kathryn@OPI2051-001.cs.clsi.ca>
2010-09-17 15:43:32 -0600

    merge

---
+++ cmcf/urls.py
@@ -0,0 +1,56 @@
+import os
+
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from sitemap import PageSitemap 
+from blog.feeds import LatestEntries
+
+admin.autodiscover()
+
+sitemaps = {
+    'pages' : PageSitemap,
+}
+
+feeds = {
+    'latest': LatestEntries,
+}
+
+urlpatterns = patterns('',
+    # Example:
+    (r'^photologue/', include('photologue.urls')),
+    (r'^grappelli/', include('grappelli.urls')),
+    (r'^admin/filebrowser/', include('filebrowser.urls')),
+    (r'^admin_tools/', include('admin_tools.urls')),
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/media/img/clslogo.ico'}),
+    (r'^wiki', include('simplewiki.urls')),
+
+    # This avoids breaking Django admin's localization JavaScript when using
+    # the FeinCMS frontend editing:
+    url(r'admin/page/page/jsi18n/',     'django.views.generic.simple.redirect_to', {'url': '/admin/jsi18n/'}),
+
+    (r'^admin/', include(admin.site.urls)),
+
+    (r'^feincms_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '/var/website/cmcf-website/cmcf/media/feincms/'}),
+#        {'document_root': os.path.join(os.path.dirname(os.path.dirname(__file__)), 'feincms/media/feincms/')}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), 'media/')}),
+
+    url(r'^preview/(?P<page_id>\d+)/', 'feincms.views.base.preview_handler', name='feincms:preview'),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+
+    # This entry is here strictly for application content testing
+    # XXX this really needs to go into a URLconf file which is only used by the
+    # application content testcases
+    #url(r'^(.*)/$', 'feincms.views.applicationcontent.handler'),
+    #url(r'^(.*)/$|^$', 'feincms.views.applicationcontent.handler'),
+
+    url(r'^$|^(.*)/$', 'feincms.views.applicationcontent.handler'),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
+
+
