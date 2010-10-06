061969e834bb8ed87c953a00920f66b0d584bccf
kathryn <kathryn@OPI2051-001.cs.clsi.ca>
2010-10-06 10:34:55 -0600

    Wiki Styling/Sticky Sidebar

--- cmcf/media/js/jquery.scroll.pack.js
+++ cmcf/media/js/jquery.scroll.pack.js
@@ -8,4 +8,4 @@
 * 
 * Version: 1.0, 12/03/2009
 -----------------------*/
-$(function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href");if($(window).scrollTop()!="0"){$(this).fadeIn("slow")}var scrollDiv=$(this);$(window).scroll(function(){if($(window).scrollTop()=="0"){$(scrollDiv).fadeOut("slow")}else{$(scrollDiv).fadeIn("slow")}});$(this).click(function(){$("html, body").animate({scrollTop:0},"slow")})}});
+$(function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href");if($(window).scrollTop()!="0"){$(this).fadeIn("slow")}var scrollDiv=$(this);$(window).scroll(function(){if($(window).scrollTop()<"60"){$(scrollDiv).fadeOut("slow")}else{$(scrollDiv).fadeIn("slow")}});$(this).click(function(){$("html, body").animate({scrollTop:0},"slow")})}});
