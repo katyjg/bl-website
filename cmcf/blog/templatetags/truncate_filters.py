40531ad0a89a93e0c81f825365393dbf50791fe0
kathryn <kathryn@vm-cmcfweb-01.cs.clsi.ca>
2011-03-17 14:22:32 -0600

    - bug fix

--- cmcf/blog/templatetags/truncate_filters.py
+++ cmcf/blog/templatetags/truncate_filters.py
@@ -1,4 +1,5 @@
 from django import template  
+
 register = template.Library()  
  
 @register.filter("truncate_chars")  
@@ -9,8 +10,8 @@ def truncate_chars(value, max_length):
     truncd_val = value[:max_length]  
     if value[max_length] != " ":  
        	rightmost_space = truncd_val.rfind(" ")  
-    if rightmost_space != -1:  
-       	truncd_val = truncd_val[:rightmost_space]  
+        if rightmost_space != -1:  
+           	truncd_val = truncd_val[:rightmost_space]  
    
     return truncd_val + "..."   
 
