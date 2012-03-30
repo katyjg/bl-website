cbe54c7a31d1ee5dd585bbe6248f394cbfcf6ea0
Kathryn Janzen <kathryn@vm-cmcfweb-01.(none)>
2012-03-30 16:36:53 -0600

    - small tweak to Issue 139 so staff can edit the schedule

--- cmcf/scheduler/views.py
+++ cmcf/scheduler/views.py
@@ -28,7 +28,7 @@ def admin_login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME)
     to the log-in page if necessary.
     """
     actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated() and u.is_superuser,
+        lambda u: u.is_authenticated() and u.is_staff,
         redirect_field_name=redirect_field_name
     )
     if function:
