eae0958fed311d901ef350524ea2c2af5c515feb
root <root@vm-cmcfweb-01.(none)>
2013-04-26 10:19:44 -0600

    - Deployed Changes on April-26-2013

--- cmcf/scheduler/migrations/0001_initial.py
+++ cmcf/scheduler/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(SchemaMigration):
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('position', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
             ('office', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('category', self.gf('django.db.models.fields.IntegerField')()),
@@ -62,6 +62,19 @@ class Migration(SchemaMigration):
         # Adding unique constraint on 'OnCall', fields ['local_contact', 'date']
         db.create_unique('scheduler_oncall', ['local_contact_id', 'date'])
 
+        # Adding model 'WebStatus'
+        db.create_table('scheduler_webstatus', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date', self.gf('django.db.models.fields.CharField')(default='', max_length=15)),
+            ('status1', self.gf('django.db.models.fields.CharField')(default='', max_length=10)),
+            ('status2', self.gf('django.db.models.fields.CharField')(default='', max_length=10)),
+            ('status3', self.gf('django.db.models.fields.CharField')(default='', max_length=10)),
+        ))
+        db.send_create_signal('scheduler', ['WebStatus'])
+
+        # Adding unique constraint on 'WebStatus', fields ['date']
+        db.create_unique('scheduler_webstatus', ['date'])
+
         # Adding model 'Stat'
         db.create_table('scheduler_stat', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -82,49 +95,50 @@ class Migration(SchemaMigration):
 
     def backwards(self, orm):
         
+        # Removing unique constraint on 'Stat', fields ['mode', 'end_date', 'last_shift']
+        db.delete_unique('scheduler_stat', ['mode', 'end_date', 'last_shift'])
+
+        # Removing unique constraint on 'Stat', fields ['mode', 'start_date', 'first_shift']
+        db.delete_unique('scheduler_stat', ['mode', 'start_date', 'first_shift'])
+
+        # Removing unique constraint on 'WebStatus', fields ['date']
+        db.delete_unique('scheduler_webstatus', ['date'])
+
+        # Removing unique constraint on 'OnCall', fields ['local_contact', 'date']
+        db.delete_unique('scheduler_oncall', ['local_contact_id', 'date'])
+
+        # Removing unique constraint on 'Visit', fields ['beamline', 'end_date', 'last_shift']
+        db.delete_unique('scheduler_visit', ['beamline_id', 'end_date', 'last_shift'])
+
+        # Removing unique constraint on 'Visit', fields ['beamline', 'start_date', 'first_shift']
+        db.delete_unique('scheduler_visit', ['beamline_id', 'start_date', 'first_shift'])
+
+        # Removing unique constraint on 'SupportPerson', fields ['first_name', 'last_name', 'email']
+        db.delete_unique('scheduler_supportperson', ['first_name', 'last_name', 'email'])
+
         # Deleting model 'Beamline'
         db.delete_table('scheduler_beamline')
 
         # Deleting model 'SupportPerson'
         db.delete_table('scheduler_supportperson')
 
-        # Removing unique constraint on 'SupportPerson', fields ['first_name', 'last_name', 'email']
-        db.delete_unique('scheduler_supportperson', ['first_name', 'last_name', 'email'])
-
         # Deleting model 'Visit'
         db.delete_table('scheduler_visit')
-
-        # Removing unique constraint on 'Visit', fields ['beamline', 'start_date', 'first_shift']
-        db.delete_unique('scheduler_visit', ['beamline_id', 'start_date', 'first_shift'])
-
-        # Removing unique constraint on 'Visit', fields ['beamline', 'end_date', 'last_shift']
-        db.delete_unique('scheduler_visit', ['beamline_id', 'end_date', 'last_shift'])
 
         # Deleting model 'OnCall'
         db.delete_table('scheduler_oncall')
 
-        # Removing unique constraint on 'OnCall', fields ['local_contact', 'date']
-        db.delete_unique('scheduler_oncall', ['local_contact_id', 'date'])
+        # Deleting model 'WebStatus'
+        db.delete_table('scheduler_webstatus')
 
         # Deleting model 'Stat'
         db.delete_table('scheduler_stat')
-
-        # Removing unique constraint on 'Stat', fields ['mode', 'start_date', 'first_shift']
-        db.delete_unique('scheduler_stat', ['mode', 'start_date', 'first_shift'])
-
-        # Removing unique constraint on 'Stat', fields ['mode', 'end_date', 'last_shift']
-        db.delete_unique('scheduler_stat', ['mode', 'end_date', 'last_shift'])
 
 
     models = {
         'scheduler.beamline': {
             'Meta': {'object_name': 'Beamline'},
             'description': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
-        },
-        'scheduler.mode': {
-            'Meta': {'object_name': 'Mode'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
         },
@@ -152,7 +166,7 @@ class Migration(SchemaMigration):
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'office': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
             'position': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         'scheduler.visit': {
@@ -164,6 +178,14 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_shift': ('django.db.models.fields.IntegerField', [], {}),
             'start_date': ('django.db.models.fields.DateField', [], {'blank': 'True'})
+        },
+        'scheduler.webstatus': {
+            'Meta': {'unique_together': "(('date',),)", 'object_name': 'WebStatus'},
+            'date': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '15'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status1': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '10'}),
+            'status2': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '10'}),
+            'status3': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '10'})
         }
     }
 
