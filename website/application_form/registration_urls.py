2b1f63e307b5ed3b4f9d7392017acca87786c099
janzen <janzen@OPI2051-001.clsi.ca>
2015-04-17 11:23:31 -0600

    - bug fix

--- website/application_form/registration_urls.py
+++ website/application_form/registration_urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from application_form.views import application_form, applicant_list, participant_list
 from application_form.forms import RegistrationForm
@@ -7,8 +8,8 @@ from application_form.models import Registration
 
 urlpatterns = patterns('',
    url(r'^$', application_form, {'model': Registration, 'form_class': RegistrationForm, 'template_name': 'application_form/registration_form.html', 'template_retry': 'application_form/registration_form_retry.html', 'success_url': 'registration_form_sent' }, name='registration_form'),
-   url(r'^sent/$', direct_to_template, { 'template': 'application_form/registration_form_sent.html' }, name='registration_form_sent'),
-   url(r'^oops/$', direct_to_template, { 'template': 'application_form/registration_form_retry.html' }, name='registration_form_retry'),
+   url(r'^sent/$', TemplateView.as_view(template_name='application_form/registration_form_sent.html'), name='registration_form_sent'),
+   url(r'^oops/$', TemplateView.as_view(template_name='application_form/registration_form_retry.html'), name='registration_form_retry'),
    url(r'^706d24997bff1968c50212f81a58e369/$', participant_list, {'template': 'application_form/participant_list.html' }, name='participant_list'),
    url(r'^a5ca37fa4d0856a4176f6355ea2ec888/$', participant_list, {'template': 'application_form/registration_abstract_list.html' }, name='registration_abstract_list')
    )
