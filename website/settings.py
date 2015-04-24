c375f221e7f524571137e0b48c8059685d156e1a
Michel Fodje <michel4j@gmail.com>
2015-04-24 16:07:50 -0600

    - bug fix

--- website/settings.py
+++ website/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'website.db'),
+        'NAME': os.path.join(BASE_DIR, 'local', 'website.db'),
     }
 }
 CACHES = {
