6b6e598a51a07f72b2786d3b852f7398dfac5035
Kathryn Janzen <janzen@OPI2051-001.clsi.ca>
2015-05-11 09:26:54 -0600

    - untesting

--- website/settings_template.py
+++ website/settings_template.py
@@ -1,6 +1,4 @@
-""" 
-Testing
-Customizing your site:
+"""Customizing your site:
     1. Save this file as settings_local.py
     2. Update settings_local.py with your information
     3. With the admin file manager, go to the site-images/prod folder and upload images for:
