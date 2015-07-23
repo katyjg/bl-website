08bfc246547beffe384f9c1cf6179f6d9bb73679
michel <michel.fodje@lightsource.ca>
2015-07-23 10:51:53 -0600

    - modified ordering

--- website/scheduler/models.py
+++ website/scheduler/models.py
@@ -82,6 +82,7 @@ class SupportPerson(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=20, help_text="Ten digit number", blank=True)
     image = models.ImageField(_('image'), blank=True, upload_to=get_storage_path)
+    display_order = models.IntegerField(default=0)
     #image = FilerImageField(blank=True, null=True)
     office = models.CharField(blank=True, max_length=50)
     category = models.IntegerField(blank=False, choices=STAFF_CHOICES)
@@ -97,6 +98,7 @@ class SupportPerson(models.Model):
         unique_together = (
             ("first_name", "last_name", "email"),
             )
+        ordering = ('display_order', )
         verbose_name_plural = "Personnel"
 
 class VisitManager(models.Manager):
