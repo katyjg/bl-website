e3930f444aab90a9230f9c5d9a4f87553667faf3
Michel Fodje <michel.fodje@lightsource.ca>
2018-08-02 10:37:52 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker build --rm -t opi2051-002:5000/cls-blweb:refresh . ; sudo docker push opi2051-002:5000/cls-blweb:refresh
-#docker rmi $(docker images -q -f "dangling=true")
+sudo docker build --rm -t srv-cmcf-dp4:5000/cls-blweb:latest . && sudo docker push srv-cmcf-dp4:5000/cls-blweb:latest
+sudo docker rmi $( sudo docker images -q -f "dangling=true")
+
